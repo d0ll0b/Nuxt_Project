4bc7706ccabaf11bc49a85dd984e8b882cceddfb
dollob <s1052782@gm.pu.edu.tw>
2024-12-21 12:11:55 +0800

    開啟訂房路由

---
+++ pages/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>會員登入</h1>
+    <form id="form">
+        <div>
+            <input type="email"id="username" placeholder="name@example.com" required autofocus>
+            <label for="username">Email address</label>
+        </div>
+        <div>
+            <input type="password" id="password" placeholder="Password">
+            <label for="password">Password</label>
+        </div>
+        <button type="submit">登入</button>
+    </form>
+    <NuxtLink to="/login/sign-up">註冊會員</NuxtLink>
+</template>
---
+++ pages/login/sign-up.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>會員註冊</h1>
+    <form id="form">
+        <div>
+            <input type="email"id="username" placeholder="name@example.com" required autofocus>
+            <label for="username">Email address</label>
+        </div>
+        <div>
+            <input type="password" id="password" placeholder="Password">
+            <label for="password">Password</label>
+        </div>
+        <button type="submit">註冊會員</button>
+    </form>
+    <NuxtLink to="/login">會員登入</NuxtLink>
+</template>
